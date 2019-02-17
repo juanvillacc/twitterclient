3d472dc86b1b7ac8ac7c807d2d4aef9c516c06bc
JUAN VILLA CORREA <juanvillacorrea@juanvillac.local>
2019-02-17 07:42:34 -0500

    Initial commit

---
+++ twitterclient/twitterclient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitterclient
+//
+//  Created by JUAN VILLA CORREA on 17/02/19.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
